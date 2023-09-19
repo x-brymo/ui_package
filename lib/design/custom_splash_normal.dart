34d220cead7473b3bfb7baf2a5c64dfe225d8680
Mahmoud Hafez Eltarqi <98850036+x-brymo@users.noreply.github.com>
2023-09-19 10:18:11 +0300

    updated-splash

---
+++ lib/design/custom_splash_normal.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+   String image;
+  Color? color;
+  
+  double? hei;
+  
+  double? wd;
+   SplashScreen({
+    Key? key,
+    required this.image,
+    required this.color,
+    this.hei,
+    this.wd,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Center(child: Image.asset(image, fit: BoxFit.cover, width:wd,height:hei)),
+        CircularProgressIndicator(
+          color:color,
+        )
+      ],
+    );
+  }
+}
